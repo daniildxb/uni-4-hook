7f84e160db125da4707dbd847276358bfa15503c
Daniil Drobot <d.b.drobot@gmail.com>
2025-03-22 20:00:40 +0100

    fix: add skeleton contract and docs

---
+++ src/HookV1.sol
@@ -0,0 +1,168 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {BaseHook} from "v4-periphery/src/utils/BaseHook.sol";
+
+import {Hooks} from "v4-core/src/libraries/Hooks.sol";
+import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
+import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+import {CurrencyLibrary, Currency} from "v4-core/src/types/Currency.sol";
+import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
+import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
+import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "v4-core/src/types/BeforeSwapDelta.sol";
+import {BalanceDelta, BalanceDeltaLibrary} from "v4-core/src/types/BalanceDelta.sol";
+import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/**
+ * @title Lending Hook
+ * @notice Hook for lending assets in Uniswap V4 pools
+ * In addition to regular hook methods, this hook takes ownership of user LP position and lends it
+ * into AAVE protocol.
+ * Hook ensures that no liquidity can be added or removed directly from the pool, only through the hook
+ */
+contract HookV1 is BaseHook, ERC4626 {
+    using PoolIdLibrary for PoolKey;
+    using CurrencyLibrary for Currency;
+    using BalanceDeltaLibrary for BalanceDelta;
+
+    // NOTE: ---------------------------------------------------------
+    // state variables should typically be unique to a pool
+    // a single hook contract should be able to service multiple pools
+    // ---------------------------------------------------------------
+
+    uint256 public tickMin;
+    uint256 public tickMax;
+    address public aavePoolAddressesProvider;
+    IERC20 public token0;
+    IERC20 public token1;
+    bool public liquidityInitialized;
+
+    constructor(
+        IPoolManager _poolManager,
+        PoolKey memory key,
+        uint256 _tickMin,
+        uint256 _tickMax,
+        address _aavePoolAddressesProvider,
+        string memory _shareName,
+        string memory _shareSymbol
+    ) 
+        BaseHook(_poolManager)
+        ERC4626(
+            IERC20(Currency.unwrap(key.currency0))
+        )
+        ERC20(
+            _shareName,
+            _shareSymbol
+        )
+    {
+        token0 = IERC20(Currency.unwrap(key.currency0));
+        token1 = IERC20(Currency.unwrap(key.currency1));
+        tickMin = _tickMin;
+        tickMax = _tickMax;
+        aavePoolAddressesProvider = _aavePoolAddressesProvider;
+    }
+
+    /**
+     * @dev overrides totalAssets to include total liquidity in both tokens
+     */
+    function totalAssets() public view override returns (uint256) {
+        return token0.balanceOf(address(this)) + token1.balanceOf(address(this));
+    }
+
+    function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
+        return Hooks.Permissions({
+            beforeInitialize: false,
+            afterInitialize: false,
+            beforeAddLiquidity: true,
+            afterAddLiquidity: true,
+            beforeRemoveLiquidity: true,
+            afterRemoveLiquidity: true,
+            beforeSwap: true,
+            afterSwap: true,
+            beforeDonate: false,
+            afterDonate: false,
+            beforeSwapReturnDelta: false,
+            afterSwapReturnDelta: false,
+            afterAddLiquidityReturnDelta: false,
+            afterRemoveLiquidityReturnDelta: false
+        });
+    }
+
+    function addLiquidity(address token, uint256 amount) public {
+        // IERC20(token).transferFrom(msg.sender, address(this), amount);
+    }
+
+    function removeLiquidity(address token, uint256 amount) public {
+        // IERC20(token).transfer(msg.sender, amount);
+    }
+
+    // -----------------------------------------------
+    // NOTE: see IHooks.sol for function documentation
+    // -----------------------------------------------
+
+    // withdraws liquidity from aave and deposits into existing position
+    function _beforeSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata, bytes calldata)
+        internal
+        override
+        returns (bytes4, BeforeSwapDelta, uint24)
+    {
+        return (BaseHook.beforeSwap.selector, BeforeSwapDeltaLibrary.ZERO_DELTA, 0);
+    }
+
+    // withdraws liquidity from the position and puts it into aave
+    function _afterSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata, BalanceDelta, bytes calldata)
+        internal
+        override
+        returns (bytes4, int128)
+    {
+        return (BaseHook.afterSwap.selector, 0);
+    }
+
+    // ensures that liquidity is only added through the hook
+    function _beforeAddLiquidity(
+        address sender,
+        PoolKey calldata key,
+        IPoolManager.ModifyLiquidityParams calldata,
+        bytes calldata
+    ) internal override returns (bytes4) {
+        require(!liquidityInitialized || sender == address(this), "Add Liquidity through Hook");
+        liquidityInitialized = true;
+        return this.beforeAddLiquidity.selector;
+    }
+
+    // moves added liquidity to the aave pools
+    function _afterAddLiquidity(
+        address sender,
+        PoolKey calldata key,
+        IPoolManager.ModifyLiquidityParams calldata params,
+        BalanceDelta delta,
+        BalanceDelta feesAccrued,
+        bytes calldata hookData
+    ) internal override returns (bytes4, BalanceDelta) {
+        return (BaseHook.beforeAddLiquidity.selector, BalanceDeltaLibrary.ZERO_DELTA);
+    }
+
+    // ensures that liquidity is only removed through the hook
+    function _beforeRemoveLiquidity(
+        address,
+        PoolKey calldata key,
+        IPoolManager.ModifyLiquidityParams calldata,
+        bytes calldata
+    ) internal override returns (bytes4) {
+        return BaseHook.beforeRemoveLiquidity.selector;
+    }
+
+    // moves remaining funds back to aave (todo is it needed if we only withdraw specific amounts ?)
+    function _afterRemoveLiquidity(
+        address,
+        PoolKey calldata key,
+        IPoolManager.ModifyLiquidityParams calldata,
+        BalanceDelta,
+        BalanceDelta,
+        bytes calldata
+    ) internal override returns (bytes4, BalanceDelta) {
+        return (BaseHook.beforeRemoveLiquidity.selector, BalanceDeltaLibrary.ZERO_DELTA);
+    }
+}
