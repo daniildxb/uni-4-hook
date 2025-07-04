93309386332e6d7fa4d5c753c55c937ea5ea252b
Daniil Drobot <d.b.drobot@gmail.com>
2025-07-04 11:47:23 +0200

    fix: update license

--- src/interfaces/IModularHook.sol
+++ src/interfaces/IModularHook.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.26;
 
 // the interface is far from complete, added it for the sake of using with sol! in rust
