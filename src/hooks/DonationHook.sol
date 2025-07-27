fc34752514ad57752aca4e9ca58d660287cea7e0
Daniil Drobot <d.b.drobot@gmail.com>
2025-07-27 17:47:56 +0200

    fix: mark DonationHook as abstract

--- src/hooks/DonationHook.sol
+++ src/hooks/DonationHook.sol
@@ -5,7 +5,7 @@ import {IERC20Metadata} from "@openzeppelin/contracts/interfaces/IERC20Metadata.
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 // Hook that accepts erc20 donations and emits event
-contract DonationHook {
+abstract contract DonationHook {
     using SafeERC20 for IERC20Metadata;
 
     event Donate(address indexed sender, address indexed token, uint256 amount);
